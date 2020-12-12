3b75fcddf41227850f2a44c91f99cd9db28d9e1e
Jackie <jackieo5023@gmail.com>
2020-12-11 23:16:01 -0800

    Adding all files

---
+++ README.md
@@ -0,0 +1,102 @@
+# Flow Coverage Compare
+
+Github action to compare flow coverage on a PR to the base branch.
+
+Running this action will create a comment like this on your pull request:
+![Example of action](assets/example.png)
+
+## Usage
+
+You'll want to add this job to one of your [workflow files](https://docs.github.com/en/actions/configuring-and-managing-workflows/configuring-a-workflow). Below are examples of sample workflow files using this action. Additionally, in order to use this action, you'll want to ensure you can run flow like `yarn run flow` or `npm run flow` in your repository.
+
+### Action inputs
+
+The possible inputs for this action are:
+
+| Parameter         | Description                                                                                                                                       | Default        |
+| ----------------- | ------------------------------------------------------------------------------------------------------------------------------------------------- | -------------- |
+| `github-token`    | The GitHub authentication token                                                                                                                   | `GITHUB_TOKEN` |
+| `package-manager` | The package manager your repo uses - either `yarn` or `npm`                                                                                       | `yarn`         |
+| `path`            | The path to your `.flowconfig`                                                                                                                    |                |
+| `pattern`         | The regex pattern used to determine which files to run flow on                                                                                    | `^.*.js$`      |
+| `threshold`       | The number of percentage points _down_ where this job will fail - not specifying this input will cause the job to always pass if run successfully |                |
+
+### Examples
+
+**"Copy paste" config**
+
+This config uses all the defaults, specifying nothing - it assumes you are using yarn, want to check all `.js` files, always want the check to pass, and just make a comment on your PR with the flow comparison.
+
+Note: The steps before `Run check` in this workflow are important! They checkout your head branch from your PR, and the base branch it wants to merge into.
+
+```yml
+name: Flow Coverage Compare
+
+on: [pull_request]
+
+jobs:
+  check:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout head branch
+        uses: actions/checkout@v2
+        with:
+          path: head
+      - name: Checkout base branch
+        uses: actions/checkout@v2
+        with:
+          path: base
+          ref: ${{ github.base_ref }}
+      - name: Install dependencies
+        run: |
+          cd head
+          yarn
+          cd ../base
+          yarn
+      - name: Run check
+        uses: jackieo5023/flow-coverage-compare@v1
+```
+
+**Customizable config**
+
+This config specifies 4 things:
+
+- You are using `npm`
+- Your flow config is under the path `client/`
+- You want to check _only_ the files under the `client/src` directory, ending in `.js`
+- You want to fail the job if any file checked has a decline in flow coverage greater than or equal to 10%
+
+Note: You still need to include the steps before `Run check` with this workflow!
+
+```yml
+name: Flow Coverage Compare
+
+on: [pull_request]
+
+jobs:
+  check:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout head branch
+        uses: actions/checkout@v2
+        with:
+          path: head
+      - name: Checkout base branch
+        uses: actions/checkout@v2
+        with:
+          path: base
+          ref: ${{ github.base_ref }}
+      - name: Install dependencies
+        run: |
+          cd head
+          npm install
+          cd ../base
+          npm install
+      - name: Run check
+        uses: jackieo5023/flow-coverage-compare@v1
+        with:
+          package-manager: npm
+          path: client/
+          pattern: ^src\/.*.js$
+          threshold: 10
+```
---
+++ index.js
@@ -0,0 +1,142 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { round } = require("lodash");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+const { context } = github;
+
+const COMMENT_HEADER = "## Flow Coverage\n";
+
+async function getFilesInPR(octokit, pattern) {
+  const res = await octokit.request(
+    "GET /repos/{owner}/{repo}/pulls/{pull_number}/files",
+    {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.pull_request.number,
+    }
+  );
+  if (res.status !== 200) {
+    throw "Cannot find PR";
+  }
+  return res.data
+    .map(({ filename, status }) => {
+      return {
+        filename,
+        status,
+      };
+    })
+    .filter(({ filename }) => {
+      const regex = new RegExp(pattern, "gmi");
+      return regex.test(filename);
+    });
+}
+
+async function getCoverageData(directory, filenames, packageManager) {
+  const outputs = await Promise.all(
+    filenames.map((filename) =>
+      exec(
+        `cd ${directory} && ${packageManager} flow coverage ${filename} --quiet`
+      )
+    )
+  );
+  const coverageData = {};
+  outputs.forEach(async ({ stdout, stderr }, index) => {
+    const begin = stdout.indexOf(": ") + 2;
+    const end = stdout.indexOf("%");
+    coverageData[filenames[index]] = stdout.substring(begin, end);
+  });
+
+  return coverageData;
+}
+
+function getMarkdownTableAndThresholdPass(coverageDifference, threshold) {
+  const floatThreshold = parseFloat(threshold);
+  let passesThreshold = true;
+  let table = "| File | Difference |\n| --- | --- |";
+  Object.keys(coverageDifference).forEach((filename) => {
+    if (isNaN(coverageDifference[filename])) {
+      table += `\n| ${filename} | ${coverageDifference[filename]}`;
+    } else {
+      const roundedDifference =
+        Math.round(
+          (parseFloat(coverageDifference[filename]) + Number.EPSILON) * 100
+        ) / 100;
+      passesThreshold =
+        passesThreshold &&
+        !isNaN(floatThreshold) &&
+        Math.sign(roundedDifference) === -1
+          ? roundedDifference > -Math.abs(threshold)
+          : passesThreshold;
+      let differenceString = roundedDifference.toString();
+      if (differenceString.charAt(0) !== "-") {
+        differenceString = "+" + differenceString;
+      }
+      table += `\n| ${filename} | ${differenceString}%`;
+    }
+  });
+
+  return { table, passesThreshold };
+}
+
+async function run() {
+  const pattern = core.getInput("pattern");
+  const myToken = core.getInput("github-token");
+  const octokit = github.getOctokit(myToken);
+  const filenamesInPR = await getFilesInPR(octokit, pattern);
+
+  const modifiedFiles = filenamesInPR
+    .filter(({ filename, status }) => status === "modified")
+    .map(({ filename }) => filename);
+  const packageManager = core.getInput("package-manager");
+  const path = core.getInput("path");
+  const prCoverageData = await getCoverageData(
+    `head${path !== "" ? `/${path}` : ""}`,
+    modifiedFiles,
+    packageManager
+  );
+  const baseCoverageData = await getCoverageData(
+    `base/${path !== "" ? `/${path}` : ""}`,
+    modifiedFiles,
+    packageManager
+  );
+
+  const coverageDifference = {};
+  modifiedFiles.forEach((filename) => {
+    coverageDifference[filename] = (
+      prCoverageData[filename] - baseCoverageData[filename]
+    ).toString();
+  });
+  filenamesInPR.forEach(({ filename, status }) => {
+    if (status !== "modified") {
+      coverageDifference[filename] = status;
+    }
+  });
+
+  const threshold = core.getInput("threshold");
+  const { table, passesThreshold } = getMarkdownTableAndThresholdPass(
+    coverageDifference,
+    threshold
+  );
+
+  await octokit.request(
+    "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+    {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: context.payload.pull_request.number,
+      body: COMMENT_HEADER + table,
+    }
+  );
+
+  if (passesThreshold === false) {
+    core.setFailed(`A file does not pass the flow threshold of ${threshold}`);
+  }
+}
+
+try {
+  run();
+} catch (error) {
+  core.setFailed(error.message);
+}
